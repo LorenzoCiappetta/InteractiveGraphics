0d7109ead9f3bd1ac7d28cfc98fbb40232fa642e
Lorenzo Ciappetta <ciappetta.2011296@studenti.uniroma1.it>
2025-03-24 13:36:56 +0100

    fixed repository problem and added project2 back

---
+++ project2_Transformations/project2.js
@@ -0,0 +1,45 @@
+// Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
+// The transformation first applies scale, then rotation, and finally translation.
+// The given rotation value is in degrees.
+function GetTransform( positionX, positionY, rotation, scale )
+{
+    //initialize result matrix
+    var res = new Array( 9 );
+    //fill rotation+scale transformation
+    res[0] = Math.cos(rotation * Math.PI / 180)*scale;
+    res[1] = Math.sin(rotation * Math.PI / 180)*scale;
+    res[2] = 0;
+    res[3] = -Math.sin(rotation * Math.PI / 180)*scale;
+    res[4] = Math.cos(rotation * Math.PI / 180)*scale;
+    res[5] = 0;
+    //fill translation transformation
+    res[6] = positionX;
+    res[7] = positionY;
+    res[8] = 1;
+    
+	return res;
+}
+
+// Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
+// The arguments are transformation matrices in the same format.
+// The returned transformation first applies trans1 and then trans2.
+function ApplyTransform( trans1, trans2 )
+{
+    //initialize result matrix
+    res = new Array( 9 );
+    //matrix multiplication
+    //first row
+    res[0] = trans2[0]*trans1[0] + trans2[3]*trans1[1] + trans2[6]*trans1[2];
+    res[3] = trans2[0]*trans1[3] + trans2[3]*trans1[4] + trans2[6]*trans1[5];
+    res[6] = trans2[0]*trans1[6] + trans2[3]*trans1[7] + trans2[6]*trans1[8];
+    //second row
+    res[1] = trans2[1]*trans1[0] + trans2[4]*trans1[1] + trans2[7]*trans1[2];
+    res[4] = trans2[1]*trans1[3] + trans2[4]*trans1[4] + trans2[7]*trans1[5];
+    res[7] = trans2[1]*trans1[6] + trans2[4]*trans1[7] + trans2[7]*trans1[8];
+    //third row
+    res[2] = trans2[2]*trans1[0] + trans2[5]*trans1[1] + trans2[8]*trans1[2];
+    res[5] = trans2[2]*trans1[3] + trans2[5]*trans1[4] + trans2[8]*trans1[5];
+    res[8] = trans2[2]*trans1[6] + trans2[5]*trans1[7] + trans2[8]*trans1[8];
+    
+	return res;
+}
